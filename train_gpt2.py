eb59c60e855abc7c925f92c6cb9d1067a5231ea7
KyuDan1 <wjdrbeks1021@naver.com>
2024-09-18 01:39:40 +0900

    2024-09-18-7

--- train_gpt2.py
+++ train_gpt2.py
@@ -367,7 +367,7 @@ def get_lr(it):
 #optimizer
 #optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
 
-optimizer = model.configure_optimizers(weight_decay = 0.1, learning_rate=6e-4, device = device)
+optimizer = raw_model.configure_optimizers(weight_decay = 0.1, learning_rate=6e-4, device = device)
 
 
 for step in range(max_steps):
